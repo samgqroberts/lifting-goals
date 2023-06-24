241579bf5c8c460eb10c0db4e8ac408c5c0a560f
Sam Roberts <samgqroberts@gmail.com>
2023-06-24 17:29:49 -0400

    fix: incorrect maths in ratios when goal is proportional to body weight

--- src/Ratios.tsx
+++ src/Ratios.tsx
@@ -1,9 +1,13 @@
-import { LiftSlice, roundTo5 } from './Lifts';
+import { useRef } from 'react';
+
+import { LiftSlice } from './Lifts';
+import { useContainerDimensions } from './useContainerDimensions';
 
 const percentify = (value: number) => `${(value * 100).toFixed(0)}%`;
 
 const Grey = '#968b8a';
 const LightRed = '#db655e';
+type Width = 'small' | 'medium' | 'goforit';
 
 const RatioRow: React.FC<{
   skipRight?: boolean;
@@ -13,13 +17,25 @@ const RatioRow: React.FC<{
   maxProportionLeft: number;
   proportionRight: number | null;
   maxProportionRight: number;
-}> = ({ skipRight = false, name, weight, proportionLeft, maxProportionLeft, proportionRight, maxProportionRight }) => {
+  width: Width;
+}> = ({
+  skipRight = false,
+  name,
+  weight,
+  proportionLeft,
+  maxProportionLeft,
+  proportionRight,
+  maxProportionRight,
+  width,
+}) => {
   const derivedProportionLeft = (proportionLeft === null ? 1 : proportionLeft) / maxProportionLeft;
   const derivedProportionRight = (proportionRight === null ? 1 : proportionRight) / maxProportionRight;
   const proportionLabelWidth = 36;
   const proportionLabelMargin = 3;
+  const fontSize = width === 'small' ? 12 : width === 'medium' ? 14 : 16;
+  const middleWidth = width === 'small' ? 82 : width === 'medium' ? 96 : 104;
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: 'flex', fontSize }}>
       {/* left */}
       <div style={{ display: 'flex', flexGrow: 1, alignItems: 'center' }}>
         <span
@@ -44,7 +60,7 @@ const RatioRow: React.FC<{
       <div
         style={{
           display: 'flex',
-          width: 92,
+          width: middleWidth,
           justifyContent: 'space-between',
           whiteSpace: 'nowrap',
           margin: '0 3px',
@@ -83,23 +99,37 @@ export const Ratios: React.FC<{
   weights: LiftSlice;
   bodyWeightAsWeight?: boolean;
 }> = ({ bodyWeight, weights, bodyWeightAsWeight = false }) => {
+  const componentRef = useRef<HTMLDivElement>(null);
+  const { width: containerWidth } = useContainerDimensions(componentRef);
+  console.log({ containerWidth });
+  const width: Width = containerWidth < 300 ? 'small' : containerWidth < 400 ? 'medium' : 'goforit';
+  const weightsInLb: LiftSlice = bodyWeightAsWeight
+    ? {
+        squat: weights.squat * bodyWeight,
+        bp: weights.bp * bodyWeight,
+        row: weights.row * bodyWeight,
+        ohp: weights.ohp * bodyWeight,
+        dl: weights.dl * bodyWeight,
+      }
+    : weights;
   const maxProportionLeft = Math.max(
     1,
-    weights.squat / bodyWeight,
-    weights.bp / bodyWeight,
-    weights.row / bodyWeight,
-    weights.ohp / bodyWeight,
-    weights.dl / bodyWeight,
+    weightsInLb.squat / bodyWeight,
+    weightsInLb.bp / bodyWeight,
+    weightsInLb.row / bodyWeight,
+    weightsInLb.ohp / bodyWeight,
+    weightsInLb.dl / bodyWeight,
   );
   const maxProportionRight = Math.max(
     1,
-    weights.bp / weights.squat,
-    weights.row / weights.squat,
-    weights.ohp / weights.squat,
-    weights.dl / weights.squat,
+    weightsInLb.bp / weightsInLb.squat,
+    weightsInLb.row / weightsInLb.squat,
+    weightsInLb.ohp / weightsInLb.squat,
+    weightsInLb.dl / weightsInLb.squat,
   );
   return (
     <div
+      ref={componentRef}
       style={{
         display: 'flex',
         flexDirection: 'column',
@@ -113,42 +143,42 @@ export const Ratios: React.FC<{
         weight={bodyWeight}
         proportionLeft={null}
         proportionRight={null}
-        {...{ maxProportionLeft, maxProportionRight }}
+        {...{ maxProportionLeft, maxProportionRight, width }}
       />
       <RatioRow
         name="Squat"
-        weight={weights.squat}
-        proportionLeft={bodyWeightAsWeight ? roundTo5(weights.squat * bodyWeight) : weights.squat / bodyWeight}
+        weight={weightsInLb.squat}
+        proportionLeft={weightsInLb.squat / bodyWeight}
         proportionRight={null}
-        {...{ maxProportionLeft, maxProportionRight }}
+        {...{ maxProportionLeft, maxProportionRight, width }}
       />
       <RatioRow
         name="Bench"
-        weight={weights.bp}
-        proportionLeft={bodyWeightAsWeight ? roundTo5(weights.bp * bodyWeight) : weights.bp / bodyWeight}
-        proportionRight={weights.bp / weights.squat}
-        {...{ maxProportionLeft, maxProportionRight }}
+        weight={weightsInLb.bp}
+        proportionLeft={weightsInLb.bp / bodyWeight}
+        proportionRight={weightsInLb.bp / weightsInLb.squat}
+        {...{ maxProportionLeft, maxProportionRight, width }}
       />
       <RatioRow
         name="Row"
-        weight={weights.row}
-        proportionLeft={bodyWeightAsWeight ? roundTo5(weights.row * bodyWeight) : weights.row / bodyWeight}
-        proportionRight={weights.row / weights.squat}
-        {...{ maxProportionLeft, maxProportionRight }}
+        weight={weightsInLb.row}
+        proportionLeft={weightsInLb.row / bodyWeight}
+        proportionRight={weightsInLb.row / weightsInLb.squat}
+        {...{ maxProportionLeft, maxProportionRight, width }}
       />
       <RatioRow
         name="OHP"
-        weight={weights.ohp}
-        proportionLeft={bodyWeightAsWeight ? roundTo5(weights.ohp * bodyWeight) : weights.ohp / bodyWeight}
-        proportionRight={weights.ohp / weights.squat}
-        {...{ maxProportionLeft, maxProportionRight }}
+        weight={weightsInLb.ohp}
+        proportionLeft={weightsInLb.ohp / bodyWeight}
+        proportionRight={weightsInLb.ohp / weightsInLb.squat}
+        {...{ maxProportionLeft, maxProportionRight, width }}
       />
       <RatioRow
         name="DL"
-        weight={weights.dl}
-        proportionLeft={bodyWeightAsWeight ? roundTo5(weights.dl * bodyWeight) : weights.dl / bodyWeight}
-        proportionRight={weights.dl / weights.squat}
-        {...{ maxProportionLeft, maxProportionRight }}
+        weight={weightsInLb.dl}
+        proportionLeft={weightsInLb.dl / bodyWeight}
+        proportionRight={weightsInLb.dl / weightsInLb.squat}
+        {...{ maxProportionLeft, maxProportionRight, width }}
       />
     </div>
   );
